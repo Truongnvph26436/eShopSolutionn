391aa9ba9f414c364fb90627864db4143d2bfd38
Trường Nguyễn <truongnvph26436@fpt.edu.vn>
2022-07-13 17:24:22 +0700

    reset

---
+++ eShopSolution.Data/Configurations/AppRoleConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class AppRoleConfigurations : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.ToTable("AppRoles");
+            
+
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+
+        }
+    }
+}
